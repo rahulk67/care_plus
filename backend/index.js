51d874e1af7665f867825c69cc964b18e2eafb85
rahulk67 <rahulkumawat6767@gmail.com>
2026-09-20 11:00:30 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ connection;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ["http://localhost:3000",],
+    origin: ["http://localhost:3000","https://care-plus-frontend-tau.vercel.app"],
     credentials: true
 }));
 app.use(express.static("public"));
--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const backUrl = process.env.BACKEND_URL || "https://care-plus-backend-alpha.vercel.app";
+const backUrl = process.env.REACT_APP_BACKEND_URL || "https://care-plus-backend-alpha.vercel.app";
 const axiosInstance = axios.create({
     baseURL: backUrl,
     headers: { "Content-Type": "application/json" }
